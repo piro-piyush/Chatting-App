74cc2e558a9543a830d1e06fed49559c12c3f791
piro-piyush <piyush72717272@gmail.com>
2024-10-15 22:37:25 +0530

    update settings

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -83,14 +83,7 @@ class _SettingsState extends State<Settings> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const Profile(),
-                  ),
-                );
-              },
+              onTap: () {Navigator.push(context, MaterialPageRoute(builder: (context) => const Profile(),),);},
               child: Container(
                 width: MediaQuery.of(context).size.width,
                 height: MediaQuery.of(context).size.height * 0.15,
@@ -111,32 +104,19 @@ class _SettingsState extends State<Settings> {
                     children: [
                       Row(
                         children: [
-                          CircleAvatar(
-                            radius: 40,
-                            backgroundImage: myPhoto != null
-                                ? CachedNetworkImageProvider(
-                                    myPhoto!) // Use CachedNetworkImageProvider for caching
-                                : const AssetImage("assets/images/default.jpeg")
-                                    as ImageProvider,
-                            // Placeholder image if myPhoto is null
+                          CircleAvatar(radius: 40, backgroundImage: myPhoto != null ? CachedNetworkImageProvider(myPhoto!) : const AssetImage("assets/images/default.jpeg") as ImageProvider,
                             child: myPhoto !=
                                     null // If myPhoto is not null, show loading indicator
-                                ? ClipOval(
-                                    child: SizedBox(
-                                      width: 80, // Size to match CircleAvatar
-                                      height: 80,
+                                ? ClipOval(child: SizedBox(width: 80,
                                       child: CachedNetworkImage(
                                         imageUrl: myPhoto!,
                                         placeholder: (context, url) =>
                                             const Center(
-                                          child:
-                                              CircularProgressIndicator(), // Loading indicator
+                                          child: CircularProgressIndicator(), // Loading indicator
                                         ),
-                                        errorWidget: (context, url, error) =>
-                                            const Icon(Icons.error),
+                                        errorWidget: (context, url, error) => const Icon(Icons.error),
                                         // Error icon
-                                        fit: BoxFit
-                                            .cover, // Fit the image inside the CircleAvatar
+                                        fit: BoxFit.cover, // Fit the image inside the CircleAvatar
                                       ),
                                     ),
                                   )
@@ -151,14 +131,12 @@ class _SettingsState extends State<Settings> {
                               Text(
                                 myName ?? 'Name not set',
                                 // Default text if null
-                                style: const TextStyle(
-                                    fontSize: 22, color: Colors.black),
+                                style: const TextStyle(fontSize: 22, color: Colors.black),
                               ),
                               Text(
                                 myUsername ?? 'Username not set',
                                 // Default text if null
-                                style: const TextStyle(
-                                    fontSize: 18, color: Colors.grey),
+                                style: const TextStyle(fontSize: 18, color: Colors.grey),
                               ),
                             ],
                           ),
@@ -167,17 +145,9 @@ class _SettingsState extends State<Settings> {
                       const Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Icon(
-                            Icons.qr_code,
-                            color: Color(0xFF008069),
-                            size: 30,
-                          ),
+                          Icon(Icons.qr_code, color: Color(0xFF008069), size: 30,),
                           SizedBox(width: 15),
-                          Icon(
-                            Icons.arrow_drop_down_circle_outlined,
-                            color: Color(0xFF008069),
-                            size: 30,
-                          ),
+                          Icon(Icons.arrow_drop_down_circle_outlined, color: Color(0xFF008069), size: 30,),
                         ],
                       ),
                     ],
@@ -185,124 +155,52 @@ class _SettingsState extends State<Settings> {
                 ),
               ),
             ),
-            buildSettingOptions(
-                titleText: 'Account',
-                subtitleText: "Security notifications, change number",
-                iconData: Icons.key),
-            buildSettingOptions(
-                titleText: 'Privacy',
-                subtitleText: "Block contacts, disappearing messages",
-                iconData: Icons.lock_outline),
-            buildSettingOptions(
-                titleText: 'Avatar',
-                subtitleText: "Create, edit, profile photo",
-                iconData: Icons.face_unlock_outlined),
-            buildSettingOptions(
-                titleText: 'Favourites',
-                subtitleText: "Add reorder, remove",
-                iconData: Icons.favorite_border_sharp),
-            buildSettingOptions(
-                titleText: 'Chats',
-                subtitleText: "Theme, wallpapers, chat history",
-                iconData: Icons.chat_outlined),
-            buildSettingOptions(
-                titleText: 'Notifications',
-                subtitleText: "Message, group & call tones",
-                iconData: Icons.notifications_none_outlined),
-            buildSettingOptions(
-                titleText: 'Storage and data',
-                subtitleText: "Network usage, auto-download",
-                iconData: Icons.data_saver_off_rounded),
-            buildSettingOptions(
-                titleText: 'App language',
-                subtitleText: "English (device's language",
-                iconData: Icons.language),
-            buildSettingOptions(
-                titleText: 'Help',
-                subtitleText: "Help centre, contact us, privacy policy",
-                iconData: Icons.help_outline_sharp),
-            buildOtherOptions(
-                titleText: "Invite a friend",
-                iconData: Icons.people_alt_outlined),
-            buildOtherOptions(
-                titleText: "App updates",
-                iconData: Icons.security_update_outlined),
-            const SizedBox(
-              height: 30,
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 18.0),
-              child: Text(
-                "Also from Meta",
-                style: TextStyle(fontSize: 14),
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            buildSocialTile(
-                titleText: "Open Instagram",
-                iconData: FontAwesomeIcons.instagram),
-            buildSocialTile(
-                titleText: "Open Facebook",
-                iconData: FontAwesomeIcons.facebook),
-            buildSocialTile(
-                titleText: "Open Threads", iconData: FontAwesomeIcons.threads),
+            buildSettingOptions(titleText: 'Account', subtitleText: "Security notifications, change number", iconData: Icons.key),
+            buildSettingOptions(titleText: 'Privacy', subtitleText: "Block contacts, disappearing messages", iconData: Icons.lock_outline),
+            buildSettingOptions(titleText: 'Avatar', subtitleText: "Create, edit, profile photo", iconData: Icons.face_unlock_outlined),
+            buildSettingOptions(titleText: 'Favourites', subtitleText: "Add reorder, remove", iconData: Icons.favorite_border_sharp),
+            buildSettingOptions(titleText: 'Chats', subtitleText: "Theme, wallpapers, chat history", iconData: Icons.chat_outlined),
+            buildSettingOptions(titleText: 'Notifications', subtitleText: "Message, group & call tones", iconData: Icons.notifications_none_outlined),
+            buildSettingOptions(titleText: 'Storage and data', subtitleText: "Network usage, auto-download", iconData: Icons.data_saver_off_rounded),
+            buildSettingOptions(titleText: 'App language', subtitleText: "English (device's language", iconData: Icons.language),
+            buildSettingOptions(titleText: 'Help', subtitleText: "Help centre, contact us, privacy policy", iconData: Icons.help_outline_sharp),
+            buildOtherOptions(titleText: "Invite a friend", iconData: Icons.people_alt_outlined),
+            buildOtherOptions(titleText: "App updates", iconData: Icons.security_update_outlined),
+            const SizedBox(height: 30,),
+            const Padding(padding: EdgeInsets.only(left: 18.0),
+              child: Text("Also from Meta", style: TextStyle(fontSize: 14),),),
+            const SizedBox(height: 10,),
+            buildSocialTile(titleText: "Open Instagram", iconData: FontAwesomeIcons.instagram),
+            buildSocialTile(titleText: "Open Facebook", iconData: FontAwesomeIcons.facebook),
+            buildSocialTile(titleText: "Open Threads", iconData: FontAwesomeIcons.threads),
           ],
         ),
       ),
     );
   }
 
-  Widget buildSettingOptions({
-    required String titleText,
-    required String subtitleText,
-    required IconData iconData,
-  }) {
+  Widget buildSettingOptions({required String titleText, required String subtitleText, required IconData iconData,}) {
     return ListTile(
       onTap: () {},
-      title: Text(
-        titleText,
-        style: const TextStyle(fontSize: 18),
-      ),
-      subtitle: Text(
-        subtitleText,
-        style: const TextStyle(fontSize: 13),
-      ),
-      leading: Icon(
-        iconData,
-        size: 24,
-      ),
+      title: Text(titleText, style: const TextStyle(fontSize: 18),),
+      subtitle: Text(subtitleText, style: const TextStyle(fontSize: 13),),
+      leading: Icon(iconData, size: 24,),
     );
   }
 
-  Widget buildOtherOptions(
-      {required String titleText, required IconData iconData}) {
+  Widget buildOtherOptions({required String titleText, required IconData iconData}) {
     return ListTile(
       onTap: () {},
-      leading: Icon(
-        iconData,
-        size: 24,
-      ),
-      title: Text(
-        titleText,
-        style: const TextStyle(fontSize: 18),
-      ),
+      leading: Icon(iconData, size: 24,),
+      title: Text(titleText, style: const TextStyle(fontSize: 18),),
     );
   }
 
-  Widget buildSocialTile(
-      {required String titleText, required IconData iconData}) {
+  Widget buildSocialTile({required String titleText, required IconData iconData}) {
     return ListTile(
       onTap: () {},
-      leading: FaIcon(
-        iconData,
-        size: 24,
-      ),
-      title: Text(
-        titleText,
-        style: const TextStyle(fontSize: 18),
-      ),
+      leading: FaIcon(iconData, size: 24,),
+      title: Text(titleText, style: const TextStyle(fontSize: 18),),
     );
   }
 }
